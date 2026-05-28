cef54515aebe28922a345f57563b7b2201aa44b6
Arup-code <chakravortyarup587@gmail.com>
2026-05-28 12:53:39 +0530

    setup commit

---
+++ src/main/java/com/dexcode/taskmasterai/TaskmasteraiApplication.java
@@ -0,0 +1,13 @@
+package com.dexcode.taskmasterai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmasteraiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmasteraiApplication.class, args);
+	}
+
+}
