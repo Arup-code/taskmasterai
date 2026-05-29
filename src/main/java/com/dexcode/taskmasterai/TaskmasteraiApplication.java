0ccafd6754eeec81aee11de48babfd5fa8de37db
Arup-code <chakravortyarup587@gmail.com>
2026-05-29 13:42:18 +0530

    chore:add dotenv java for the application

---
+++ src/main/java/com/dexcode/taskmasterai/TaskmasteraiApplication.java
@@ -0,0 +1,22 @@
+package com.dexcode.taskmasterai;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class TaskmasteraiApplication {
+
+    public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        try {
+            dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+        } catch (Exception e) {
+            log.error("Error loading .env file: {}", e.getMessage());
+        }
+        SpringApplication.run(TaskmasteraiApplication.class, args);
+    }
+
+}
